b45969a44398aa9c950be6476acf720f8e13ec42
Sdev237 <sdevsap@gmail.com>
2024-03-19 00:26:11 -0400

    add home page and language toggle

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,60 @@
 import React from 'react';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import './Home.scss';
 
 const Home = () => {
   return (
-    <div className="home" style={{height: '50vh'}}>
-      <h2>Welcome to My Website</h2>
-      <p>This is the homepage content.</p>
+    <div className="home" >
+      <Container>
+        <Row className="mt-5">
+          <Col md={3} className='border mx-2'>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Jedeon Sap</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Développeur Full Stack</Card.Subtitle>
+                <Card.Text>
+                  Ville-Marie, Montréal, QC
+                  <br />
+                  jedeon@example.com
+                </Card.Text>
+              </Card.Body>
+            </Card>
+
+            <h5>Langues</h5>
+            <ul>
+              <li>Français (Langue Maternel)</li>
+              <li>Anglais (Deuxieme Langue)</li>
+            </ul>
+            <h5>Liens utiles</h5>
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/jeremie-barriere">LinkedIn</a>
+              </li>
+              <li>
+                <a href="https://github.com/jeremie-barriere">GitHub</a>
+              </li>
+            </ul>
+          </Col>
+
+          <Col md={8} className='border mx-2'>
+            <h1>BIENVENUE SUR MON PORTFOLIO</h1>
+            <p>
+              Je suis un développeur passionné par les technologies web. J'ai travaillé sur divers projets, notamment des applications React et des API Node.js.
+            </p>
+            <hr />
+            <Row className="mt-5">
+              {['ReactJS', 'Node.js', 'Next.js', 'TailwindCSS', 'Storybook'].map((competence) => (
+                <Col md={2} key={competence}>
+                  <img src={`chemin/vers/logo-${competence}.svg`} alt={competence} />
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        </Row>
+
+        
+      </Container>
       
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,34 +2,54 @@ import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faSun, faMoon, faUser, faHome  } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.scss';
 
 const Navbar = ({ toggleTheme }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [langue, setLangue] = useState('fr');
 
   const toggle = () => {
     setIsDarkMode(!isDarkMode);
   };
 
+  const toggleFlag = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const toggleLangue = (nouvelleLangue) => {
+    setLangue(nouvelleLangue);
+    let translations;
+    if (nouvelleLangue === 'fr') {
+      
+    } else if (nouvelleLangue === 'en') {
+      
+    }
+    // Mettre à jour le contenu de votre application avec les traductions chargées
+  };
+
   const isMobile = useMediaQuery({ maxWidth: 768 }); 
 
   return (
-    <div className='nav'>
-      
-      <nav className={`navbar ${isMobile ? 'mobile' : ''}`}>
+    <section className='nav'>
+      <nav className={`navi ${isMobile ? 'mobile' : ''}`}>
         <div className="navbar-container">
-          <h1>SAP JEDEON</h1>
           <div>
-            <Link to="/">Accueil</Link>
-            <Link to="/profile">Profil</Link>
-            <a onClick={() => {toggle();toggleTheme();}} className='toggle'>
-              {isDarkMode ? (<FontAwesomeIcon icon={faSun} className='sun'/> ) : ( <FontAwesomeIcon icon={faMoon} className='moon'/> )}
+            <Link to="/"><FontAwesomeIcon icon={faHome} /></Link>
+            <Link to="/profile"><FontAwesomeIcon icon={faUser} /></Link>
+
+            <span onClick={() => {toggleFlag();toggleTheme();}} className='toggle' style={{ width: '24px', height: '20px', cursor: 'pointer' }}>
+              {isDarkMode ? (<span><img src="https://fonts.gstatic.com/s/e/notoemoji/15.0/1f1eb_1f1f7/512.png=s30" alt="Drapeau de la France" style={{ width: '24px', height: '20px'}} /> Fr</span>) : 
+              ( <span><img src="https://fonts.gstatic.com/s/e/notoemoji/15.0/1f1ec_1f1e7/512.png=s30" alt="Drapeau de l'Angleterre'" style={{ width: '24px', height: '20px' }} /> En</span> )}
+            </span>
+
+            <a onClick={() => {toggle();toggleTheme();}} className='toggle btn px-3'>
+              {isDarkMode ? (<FontAwesomeIcon icon={faSun} className='sun'/> ) : ( <FontAwesomeIcon icon={faMoon} className='moon'/> )} Theme
             </a>
           </div>
         </div>
       </nav>
-    </div>
+    </section>
   );
 };
 
